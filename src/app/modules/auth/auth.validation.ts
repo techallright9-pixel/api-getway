53efe8dc0a499df6e3105edbae76a4070c1e5bff
Mahmud2640 <mdmahmud484@gmail.com>
2026-05-08 00:29:33 +0600

    email added

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 const loginZodSchema = z.object({
     body: z.object({
-        id: z.string({
-            required_error: 'ID is required',
+        idOrEmail: z.string({
+            required_error: 'ID or Email is required',
         }),
         password: z.string({
             required_error: 'Password is required',
